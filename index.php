382ce8fa3f556142ffee1fd127449ad2864c85ec
Giordano de Brito <megagamesb@gmail.com>
2024-12-31 20:56:37 -0300

    Switch order of functions

--- index.php
+++ index.php
@@ -4,11 +4,11 @@
 
 header('Access-Control-Allow-Methods: POST, GET');
 
-require 'core/autoloader.php';
-require 'routes/routing.php';
-
 session_start();
 
 date_default_timezone_set('America/Fortaleza');
+
+require 'core/autoloader.php';
+require 'routes/routing.php';
 
 ?>
